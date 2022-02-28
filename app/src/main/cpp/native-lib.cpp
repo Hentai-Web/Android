0f1dce584ebb6091013d76f2fccb374fac1eb0cf
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-02-28 14:02:05 +0100

    N/A

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -12,11 +12,10 @@ Java_com_yourorg_sample_MainActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
-//node's libUV requires all arguments being on contiguous memory.
-extern "C" jint JNICALL
-Java_com_yourorg_sample_MainActivity_startNodeWithArguments(
+extern "C" jobject JNICALL
+Java_com_dergoogler_hentai_tools_Lib_startNodeWithArguments(
         JNIEnv *env,
-        jobject /* this */,
+        jclass clazz,
         jobjectArray arguments) {
 
     //argc
@@ -24,24 +23,25 @@ Java_com_yourorg_sample_MainActivity_startNodeWithArguments(
 
     //Compute byte size need for all arguments in contiguous memory.
     int c_arguments_size = 0;
-    for (int i = 0; i < argument_count ; i++) {
-        c_arguments_size += strlen(env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0));
+    for (int i = 0; i < argument_count; i++) {
+        c_arguments_size += strlen(
+                env->GetStringUTFChars((jstring) env->GetObjectArrayElement(arguments, i), 0));
         c_arguments_size++; // for '\0'
     }
 
     //Stores arguments in contiguous memory.
-    char* args_buffer=(char*)calloc(c_arguments_size, sizeof(char));
+    char *args_buffer = (char *) calloc(c_arguments_size, sizeof(char));
 
     //argv to pass into node.
-    char* argv[argument_count];
+    char *argv[argument_count];
 
     //To iterate through the expected start position of each argument in args_buffer.
-    char* current_args_position=args_buffer;
+    char *current_args_position = args_buffer;
 
     //Populate the args_buffer and argv.
-    for (int i = 0; i < argument_count ; i++)
-    {
-        const char* current_argument = env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0);
+    for (int i = 0; i < argument_count; i++) {
+        const char *current_argument = env->GetStringUTFChars(
+                (jstring) env->GetObjectArrayElement(arguments, i), 0);
 
         //Copy current argument to its expected position in args_buffer
         strncpy(current_args_position, current_argument, strlen(current_argument));
@@ -50,10 +50,136 @@ Java_com_yourorg_sample_MainActivity_startNodeWithArguments(
         argv[i] = current_args_position;
 
         //Increment to the next argument's expected position.
-        current_args_position += strlen(current_args_position)+1;
+        current_args_position += strlen(current_args_position) + 1;
     }
 
     //Start node, with argc and argv.
-    return jint(node::Start(argument_count,argv));
+    return reinterpret_cast<jobject>(jint(node::Start(argument_count, argv)));
 
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getReleaseURl(JNIEnv *env, jclass clazz) {
+    std::string data = "https://hw.dergoogler.com";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getDebugURl(JNIEnv *env, jclass clazz) {
+    std::string data = "http://192.168.178.81:5500/";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getInterfaceName(JNIEnv *env, jclass clazz) {
+    std::string data = "Android";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getUserAgent(JNIEnv *env, jclass clazz) {
+    std::string data = "HENTAI_WEB_AGENT";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getStorageKey(JNIEnv *env, jclass clazz) {
+    std::string data = "localstorage";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getFolderPath(JNIEnv *env, jclass clazz) {
+    std::string data = "/hentai-web/";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getFingerprintPrefKey(JNIEnv *env, jclass clazz) {
+    std::string data = "useFingerPrintToLogin";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getLanguagePrefKey(JNIEnv *env, jclass clazz) {
+    std::string data = "language";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_en(JNIEnv *env, jclass clazz) {
+    std::string data = "en";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getTrue(JNIEnv *env, jclass clazz) {
+    std::string data = "true";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getBiometricPromptTitle(JNIEnv *env, jclass clazz) {
+    std::string data = "Biometric login for Hentai Web";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getBiometricPromptSubTitle(JNIEnv *env, jclass clazz) {
+    std::string data = "Log in using your biometric credential";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_getBiometricPromptButtonTitle(JNIEnv *env, jclass clazz) {
+    std::string data = "Use password";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_SCHEME_1HTTP(JNIEnv *env, jclass clazz) {
+    std::string data = "http://";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_SCHEME_1HTTPS(JNIEnv *env, jclass clazz) {
+    std::string data = "https://";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_SCHEME_1FILE(JNIEnv *env, jclass clazz) {
+    std::string data = "file://";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_SCHEME_1ASSET(JNIEnv *env, jclass clazz) {
+    std::string data = "file://android_asset";
+    return env->NewStringUTF(data.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_dergoogler_hentai_tools_Lib_SCHEME_1RES(JNIEnv *env, jclass clazz) {
+    std::string data = "file://android_res";
+    return env->NewStringUTF(data.c_str());
 }
--- app/src/main/java/com/dergoogler/hentai/tools/Lib.java
+++ app/src/main/java/com/dergoogler/hentai/tools/Lib.java
@@ -6,25 +6,60 @@ public class Lib {
         System.loadLibrary("node");
     }
 
-    public static String getReleaseURl = "https://hw.dergoogler.com";
-    public static String getDebugURl = "http://192.168.178.81:5500/";
-    public static String getInterfaceName = "Android";
-    public static String getUserAgent = "HENTAI_WEB_AGENT";
-    public static String getStorageKey = "localstorage";
-    public static String getFolderPath = "/hentai-web/";
-    public static String getFingerprintPrefKey = "useFingerPrintToLogin";
-    public static String getLanguagePrefKey = "language";
-    public static String en = "en";
-    public static String getTrue = "true";
-    public static String getBiometricPromptTitle = "Biometric login for Hentai Web";
-    public static String getBiometricPromptSubTitle = "Log in using your biometric credential";
-    public static String getBiometricPromptButtonTitle = "Use password";
-    public static String SCHEME_HTTP = "http://";
-    public static String SCHEME_HTTPS = "https://";
-    public static String SCHEME_FILE = "file://";
-    public static String SCHEME_ASSET = "file://android_asset";
-    public static String SCHEME_RES = "file://android_res";
+    public static String getReleaseURl = getReleaseURl();
+    public static String getDebugURl = getDebugURl();
+    public static String getInterfaceName = getInterfaceName();
+    public static String getUserAgent = getUserAgent();
+    public static String getStorageKey = getStorageKey();
+    public static String getFolderPath = getFolderPath();
+    public static String getFingerprintPrefKey = getFingerprintPrefKey();
+    public static String getLanguagePrefKey = getLanguagePrefKey();
+    public static String en = en();
+    public static String getTrue = getTrue();
+    public static String getBiometricPromptTitle = getBiometricPromptTitle();
+    public static String getBiometricPromptSubTitle = getBiometricPromptSubTitle();
+    public static String getBiometricPromptButtonTitle = getBiometricPromptButtonTitle();
+    public static String SCHEME_HTTP = SCHEME_HTTP();
+    public static String SCHEME_HTTPS = SCHEME_HTTPS();
+    public static String SCHEME_FILE = SCHEME_FILE();
+    public static String SCHEME_ASSET = SCHEME_ASSET();
+    public static String SCHEME_RES = SCHEME_RES();
 
+    private native static String getReleaseURl();
 
-    public native static Integer startNodeWithArguments(String[] arguments);
+    private native static String getDebugURl();
+
+    private native static String getInterfaceName();
+
+    private native static String getUserAgent();
+
+    private native static String getStorageKey();
+
+    private native static String getFolderPath();
+
+    private native static String getFingerprintPrefKey();
+
+    private native static String getLanguagePrefKey();
+
+    private native static String en();
+
+    private native static String getTrue();
+
+    private native static String getBiometricPromptTitle();
+
+    private native static String getBiometricPromptSubTitle();
+
+    private native static String getBiometricPromptButtonTitle();
+
+    private native static String SCHEME_HTTP();
+
+    private native static String SCHEME_HTTPS();
+
+    private native static String SCHEME_FILE();
+
+    private native static String SCHEME_ASSET();
+
+    private native static String SCHEME_RES();
+
+    public native static Object startNodeWithArguments(String[] arguments);
 }
